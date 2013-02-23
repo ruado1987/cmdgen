0fc9a7b2f099855701d435ece52ca00421476305
ruavang <ruavang@ruavang-laptop>
2013-02-23 23:28:25 +0800

    Add "use strict" to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,24 @@
-module.exports = function(grunt) {
+exports.function = module(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {      
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
-	nodeunit: {
-	  all: ['test/*test.js']
-	}
-  });
+    "use strict";
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        nodeunit: {
+            all: ['test/*test.js']
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Default task(s).
-  grunt.registerTask('default', ['nodeunit', 'uglify']);
+    // Default task(s).
+    grunt.registerTask('default', ['nodeunit', 'uglify']);
 
 };
