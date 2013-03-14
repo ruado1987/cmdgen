3ac948e565baa4f28328292d4c923041595d6c6d
Harry <ruado1987@gmail.com>
2013-03-14 18:30:32 +0800

    Adds Controller controlling execution of batch of commands

--- src/argsbuilder.js
+++ src/argsbuilder.js
@@ -1,35 +1,49 @@
-exports.build = function (command) {
-    var regex = /(dply_app_push)\s(.+?)\s(.+?)\s(.+?)\s([01]{1})/g,
-        match,
-        args = [],
-        count = 0,
-        fn;
+exports.build =	function( command ) {
+	var	fn, match,
+		regex =	/(dply_app_push)\s(.+?)\s(.+?)\s(.+?)\s([01]{1})/g,		
+		commands = [],
+		count =	0;
 
-    fn = function (match) {
-        return function (exec) {
-            var args,
-                push = exec || require("child_process").exec;
+	fn = function( match ) {
+		return function( exec, controller ) {
+			var	args,				
+				push = exec	|| require( "child_process" ).exec;
 
-            args = Array.prototype.slice.call(match, 1);
-            args.unshift("/c");
-            args.unshift("cmd");
-            push(args.join(" "), function (stdin, stderr, error) {
-                console.log(stdin);
-                console.log();
-                console.log(stderr);
-                console.log();
-                if (error) {
-                    console.log("Error occurred: " + error);
-                }
-            });
-        };
-    };
+			args = Array.prototype.slice.call( match, 1 );
+			args.unshift("/c");
+			args.unshift("cmd");
+			push(args.join(" "), function ( stdin, stderr, error ) {
+				console.log( stdin + "\n" );
+				console.log( stderr+ "\n" );			
+				if ( error && error.code ) {
+					console.log( "Error occurred:\n" + error.code );
+				}
+				controller.done();
+			});			
+		};
+	};
 
-    while ((match = regex.exec(command)) !== null) {
-        args[count++] = {
-            execute: fn(match)
-        };
-    }
+	while ((match = regex.exec( command ))) {
+		commands[ count++ ] =	{
+			execute: fn( match )
+		};
+	}
 
-    return args;
+	return commands;
+};
+exports.Controller = function( commands ) {
+	return {
+		idx: 0,
+		cmds: commands,
+		done: function() {
+			var cmds = this.cmds,
+				idx = this.idx;
+			if ( idx < cmds.length - 1 ) {
+				cmds[ this.idx++ ].execute( null, this );
+			}			
+		},
+		executeBatch: function() {
+			this.cmds[ 0 ].execute( null, this );	
+		}
+	};
 };
--- test/argsbuilder.js
+++ test/argsbuilder.js
@@ -1,15 +1,39 @@
 var argsbuilder = require("../src/argsbuilder.js");
 
-exports.testBuildArgumentsFromCommand = function (test) {
+exports.testBuildArgumentsFromCommand = function( test ) {
+	test.expect( 2 );
+	
 	var data = "dply_app_push Action a/b/c.class " +
                 "\"/home/ruavang/test/Web App\\bin\\a\\b\\c.class\" 0";
-		cmds = argsbuilder.build(data);
+		cmds = argsbuilder.build(data),
+		controller = {
+			done: function() {
+				test.ok(true);	
+			}	
+		};
 
     (function (cmd) {
-        cmd.execute(function (script) {
+        cmd.execute(function (script, callback) {
             test.equal("cmd /c " + data, script);
-        });
+            callback();
+        }, controller);
 
         test.done();
     })(cmds[0]);
+};
+
+exports.testExecuteBatchOfCommand = function( test ) {
+	test.expect( 4 );
+	
+	var cmd = {
+			execute: function( script, controller ) {
+				test.ok( true );
+				controller.done();	
+			}
+		},
+		commands = [ cmd, cmd, cmd, cmd ],
+		controller = new argsbuilder.Controller( commands );
+		
+	controller.executeBatch();
+	test.done();
 };
