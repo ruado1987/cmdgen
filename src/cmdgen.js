1505e71e3a51ce23f8b6e76e30e10e56aa8feaf6
Harry <ruado1987@gmail.com>
2013-04-04 14:46:54 +0800

    Removes node 0.9 in travis.yml and formats the cmdgen.js file

--- src/cmdgen.js
+++ src/cmdgen.js
@@ -3,7 +3,7 @@ var fs = require("fs"),
     argv = require("optimist").argv,
     _ = require("underscore"),
     _s = require("underscore.string");
-    
+
 // Array.prototype methods quick reference
 var concat = Array.prototype.concat;
 
@@ -16,143 +16,145 @@ _.templateSettings = {
 
 // configuration vars
 var config = {
-        "vrl-web-app": {
-            bin: "Web Content\\WEB-INF\\classes"
-        },
-        "vrl-ejb-app": {
-            bin: "bin"
-        },
-        "vrl-dao-app": {
-            bin: "bin"
-        },
-        "vrl-commons": {
-            bin: "bin"
-        },
-        "vrl-j2ee-client": {
-            bin: "bin"
-        }
+    "vrl-web-app": {
+        bin: "Web Content\\WEB-INF\\classes"
     },
+    "vrl-ejb-app": {
+        bin: "bin"
+    },
+    "vrl-dao-app": {
+        bin: "bin"
+    },
+    "vrl-commons": {
+        bin: "bin"
+    },
+    "vrl-j2ee-client": {
+        bin: "bin"
+    }
+},
     baseDir = argv.baseDir || process.cwd(),
     appTmpl = _.template("dply_app_push ${comType} ${comPath} ${localPath} 0"),
     batchTmpl = _.template("dply_batch_push ${comType} ${comPath} ${localPath}");
 
-function getCommonDir( cType ) {
-    if ( cType === "vrl-commons" ) {
+function getCommonDir(cType) {
+    if (cType === "vrl-commons") {
         return "Framework_src";
-    } else if ( cType === "vrl-j2ee-client" ) {
+    } else if (cType === "vrl-j2ee-client") {
         return "BatchPrograms_src";
     } else {
         return "WebApplication_src";
     }
 }
-	
+
 function BatchCommandGenerator() {
-	
-	this.generateCommand = function( match ) {
-		var pComps,
-			cPath,
-			cType;
-			
-	    pComps = [
-			baseDir,
-			getCommonDir( match[1] ),
-			match[1],
-			config[match[1]].bin,
-			match[3],
-			match[4].replace("java", "class")
-		];
-	    cPath = match[3].replace(/\\/g, "/");
-	    cPath = _.join( "/", cPath, pComps[5] );
-	    cType = match[1] + ".jar";
-	    
-		return batchTmpl({
-			comType: cType,
-			comPath: cPath,
-			localPath: _s.quote(pComps.join("\\"))
-		});
-	};
+
+    this.generateCommand = function (match) {
+        var pComps,
+            cPath,
+            cType;
+
+        pComps = [
+            baseDir,
+            getCommonDir(match[1]),
+            match[1],
+            config[match[1]].bin,
+            match[3],
+            match[4].replace("java", "class")
+        ];
+        cPath = match[3].replace(/\\/g, "/");
+        cPath = _.join("/", cPath, pComps[5]);
+        cType = match[1] + ".jar";
+
+        return batchTmpl({
+            comType: cType,
+            comPath: cPath,
+            localPath: _s.quote(pComps.join("\\"))
+        });
+    };
 }
 
 function OnlineCommandGenerator() {
-	
-	this.generateCommand = function( match ) {	
-	    if ( match[5] === "java" ) {
-	        return generateCmdForJavaComponent( match );
-	    } else {
-	        return generateCmdForWebComponent( match );
-	    }
-	};
-	
-	function generateCmdForJavaComponent( match ) {
-	    var pComps, cPath, cType;
-	    pComps = [
-			baseDir,
-			getCommonDir( match[1] ),
-			match[1],
-			config[match[1]].bin,
-			match[3],
-			match[4].replace("java", "class")
-		];
-	    cPath = match[3].replace(/\\/g, "/");
-	    cPath = _.join( "/", cPath, pComps[5] );
-	    cType = match[1] === "vrl-web-app" ? "Action" : match[1] + ".jar";
-	
-	    return fillTemplate(cType, cPath, pComps);
-	}
-	
-	function generateCmdForWebComponent(match) {
-	    var pComps, cPath, cType;
-	    pComps = [
-			baseDir,
-			getCommonDir(),
-			match[1],
-			match[2],
-			match[3],
-			match[4]
-		];	
-		
-	    if (["properties", "jsp"].indexOf(match[5]) === -1) {
-	        cPath = match[3].replace(/\\/g, "/");
-	        cPath = _.join( "/", cPath, match[4] );
-	        cType = "WAR";
-	    } else if ("jsp" === match[5]) {
-	        cPath = _s.splice(match[3], 0, 4).replace(/\\/g, "/");
-	        cPath = _.join( "/", cPath, match[4] );
-	        cType = "JSP";
-	    } else { // properties
-	        cPath = match[4];
-	        cType = "Action";
-	    }
-	
-	    /* remove falsy values, which may be the case when component type is JSP */
-	    return fillTemplate(cType, cPath, _.compact(pComps));
-	}
-	
-	function fillTemplate(cType, cPath,	pComps)	{
-		if ( cType.indexOf("vrl-j2ee-client") >= 0 ) {
-			return batchTmpl({
-				comType: cType,
-				comPath: cPath,
-				localPath: _s.quote(pComps.join("\\"))
-			});
-		} else {
-			return appTmpl({
-				comType: cType,
-				comPath: cPath,
-				localPath: _s.quote(pComps.join("\\"))
-			});
-		}
-	}
+
+    this.generateCommand = function (match) {
+        if (match[5] === "java") {
+            return generateCmdForJavaComponent(match);
+        } else {
+            return generateCmdForWebComponent(match);
+        }
+    };
+
+    function generateCmdForJavaComponent(match) {
+        var pComps, cPath, cType;
+        pComps = [
+           baseDir,
+           getCommonDir(match[1]),
+           match[1],
+           config[match[1]].bin,
+           match[3],
+           match[4].replace("java", "class")
+        ];
+        cPath = match[3].replace(/\\/g, "/");
+        cPath = _.join("/", cPath, pComps[5]);
+        cType = match[1] === "vrl-web-app" ? "Action" : match[1] + ".jar";
+
+        return fillTemplate(cType, cPath, pComps);
+    }
+
+    function generateCmdForWebComponent(match) {
+        var pComps, cPath, cType;
+        pComps = [
+           baseDir,
+           getCommonDir(),
+           match[1],
+           match[2],
+           match[3],
+           match[4]
+        ];
+
+        if (["properties", "jsp"].indexOf(match[5]) === -1) {
+            cPath = match[3].replace(/\\/g, "/");
+            cPath = _.join("/", cPath, match[4]);
+            cType = "WAR";
+        } else if ("jsp" === match[5]) {
+            cPath = _s.splice(match[3], 0, 4).replace(/\\/g, "/");
+            cPath = _.join("/", cPath, match[4]);
+            cType = "JSP";
+        } else { // properties
+            cPath = match[4];
+            cType = "Action";
+        }
+
+        /* remove falsy values, which may be the case when component type is JSP */
+        return fillTemplate(cType, cPath, _.compact(pComps));
+    }
+
+    function fillTemplate(cType, cPath, pComps) {
+        if (cType.indexOf("vrl-j2ee-client") >= 0) {
+            return batchTmpl({
+                comType: cType,
+                comPath: cPath,
+                localPath: _s.quote(pComps.join("\\"))
+            });
+        } else {
+            return appTmpl({
+                comType: cType,
+                comPath: cPath,
+                localPath: _s.quote(pComps.join("\\"))
+            });
+        }
+    }
 }
 
 function genAndWriteToFile(src, dest) {
     fs.readFile(src, "utf-8", function (err, data) {
-        var pushCmds = genFromString(data);
+		if (err) {
+			return;
+		}
+        var pushCmds = genFromString(data),
+            out = fs.createWriteStream(dest);
 
-        var out = fs.createWriteStream( dest );
-
-        pushCmds.forEach(function (val, idx) {
-            out.write(val + "\n");            
+        pushCmds.forEach(function (val) {
+            out.write(val + "\n");
         });
         out.end();
         out.destroy();
@@ -161,24 +163,23 @@ function genAndWriteToFile(src, dest) {
 
 function genFromString(str, batch) {
     var match, ext, path, generator,
-		regex = /[\S]+?\\([^\\]+?)\\([^\\]+?)(?:\\([^\.]+)\\|\\)(.+)/g,		
-		exts = [ "java", "tld", "jsp", "js", "xml", "properties", "css" ],
-		slashes = [ "/", "\\" ],
+        regex = /[\S]+?\\([^\\]+?)\\([^\\]+?)(?:\\([^\.]+)\\|\\)(.+)/g,
+        exts = ["java", "tld", "jsp", "js", "xml", "properties", "css"],
+        slashes = ["/", "\\"],
         pushCmds = [];
-	
-	if (batch === true) {
-		generator = new BatchCommandGenerator();	
-	} else {
-		generator = new OnlineCommandGenerator();	
-	}
 
-    while ( (match = regex.exec(str)) ) {
-		path = match[ 0 ];				
-		if ( slashes.indexOf( path[0] ) < 0 && exts.indexOf( (ext = path.substring( path.lastIndexOf(".") + 1 )) ) >= 0 ) {
-			pushCmds.push(generator.generateCommand( concat.call(match, ext) ));
-		}
+    if (batch === true) {
+        generator = new BatchCommandGenerator();
+    } else {
+        generator = new OnlineCommandGenerator();
     }
 
+    while ((match = regex.exec(str))) {
+        path = match[0];
+        if (slashes.indexOf(path[0]) < 0 && exts.indexOf((ext = path.substring(path.lastIndexOf(".") + 1))) >= 0) {
+            pushCmds.push(generator.generateCommand(concat.call(match, ext)));
+        }
+    }
     return pushCmds;
 }
 
